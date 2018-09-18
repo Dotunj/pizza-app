d2d15284097aa038564b3f05eb8097ac6f63ae19
dotunj <dotunjolaosho@yahoo.com>
2018-09-18 12:17:26 +0100

    fixed variable name

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -9,12 +9,12 @@ class PizzaController extends Controller
 {
     public function index()
     {
-        $pizza = Pizza::with('toppings')->inRandomOrder()->get();
+        $pizzas = Pizza::with('toppings')->inRandomOrder()->get();
 
         $result = [
             'status'=>true,
             'message'=>'Retrieved all pizzas successfully',
-            'data'=>$pizza
+            'data'=>$pizzas
         ];
 
         return response()->json($result, 200);
